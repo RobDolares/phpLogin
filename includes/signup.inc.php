ffa2feed596e10e9f6270801e1d9f8cf6bfa8466
Robert Howington <zerorob2787@gmail.com>
2019-01-10 22:20:04 -0500

    signup - check if logincreds exist/insert into dB - preHASH

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -33,4 +33,39 @@ if (isset($_POST['signup-submit'])) {
     header("Location: ../signup.php?error=passwordcheck&uid=".$username."&mail=".$email);
     exit();
   }
+  //check database to see if username exists already
+  else {
+
+    $sql = "SELECT uidUsers FROM users WHERE uidUsers=? ";
+    $stmt = mysqli_stmt_init($conn);
+    // check if $stmt can be run in database($sql)
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+      header("Location: ../signup.php?error=sqlerror");
+      exit();
+    }
+    else {
+      mysqli_stmt_bind_param($stmt, "s", $username);
+      mysqli_stmt_execute($stmt);
+      mysqli_stmt_store_result($stmt);
+      $resultCheck = mysqli_stmt_num_rows($stmt);
+      if ($resultCheck > 0) {
+        header("Location: ../signup.php?error=usertaken&mail=".$email);
+        exit();
+      }
+      else {
+        $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?,?,?)";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+          header("Location: ../signup.php?error=sqlerror");
+          exit();
+        }
+        else {
+          mysqli_stmt_bind_param($stmt, "sss", $username, $email, $password);
+          mysqli_stmt_execute($stmt);
+          mysqli_stmt_store_result($stmt);
+        }
+      }
+    }
+
+  }
 }
